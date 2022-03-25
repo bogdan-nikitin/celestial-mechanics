30122da541e5fc6062d1b892dffca04c5f1c5b23
bogdan-nikitin <nikitin.bogdan333@yandex.ru>
2022-03-25 19:50:58 +0300

    Add a forgotten file

---
+++ celestial_body_object.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+from typing import List
+
+from simulation import CelestialBody
+
+
+@dataclass
+class CelestialBodyTrajectory:
+    x: List[float]
+    y: List[float]
+
+
+@dataclass
+class CelestialBodyObject:
+    def __post_init__(self):
+        self.trajectory = CelestialBodyTrajectory(
+            [self.body.pos[0]], [self.body.pos[1]]
+        )
+
+    body: CelestialBody
+    name: str
+    color: (int, int, int)
+    size: int
+    trajectory: CelestialBodyTrajectory = None
+
+
+def update_trajectories(body_objects):
+    for body_obj in body_objects:
+        body_obj.trajectory.x += [body_obj.body.pos[0]]
+        body_obj.trajectory.y += [body_obj.body.pos[1]]
